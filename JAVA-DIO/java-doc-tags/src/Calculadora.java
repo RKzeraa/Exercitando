0679efcfbdc4ad3b093724558bf54b0b0fb633a1
Ramom Karllos <rkzeraa@gmail.com>
2022-12-27 16:09:59 -0300

    [Feat] - Added New Class and [Docs] - Exploring javadoc

---
+++ JAVA-DIO/java-doc-tags/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ *  <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Ramom Karllos
+ * @version 1.0
+ * @since 27/12/2022
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizadao para somar dois números inteiros
+     * @param numberOne este é o primeiro parâmetro do método
+     * @param numberTwo este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+    public int somar(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+}
