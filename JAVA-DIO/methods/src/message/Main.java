1bb4118d49b8915cc39b78b4128018ff18aa671c
Ramom Karllos <rkzeraa@gmail.com>
2023-01-18 17:10:05 -0300

    [Feat] - New Class Message in methods

---
+++ JAVA-DIO/methods/src/message/Main.java
@@ -0,0 +1,11 @@
+package br.message;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Message");
+        Message.getMessage(9);
+        Message.getMessage(14);
+        Message.getMessage(1);
+    }
+}
